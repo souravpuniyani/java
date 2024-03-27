9b8d853f172f6ea4ab1c751854062d3efcf6f432
souravpuniyani <souravpuniyani22@gmail.com>
2024-03-27 20:34:17 +0530

    java code

---
+++ subtract.java
@@ -0,0 +1,20 @@
+package introduction;
+
+
+class subtract
+{
+ public static void main(String args[])
+ 
+  { int a=40;
+  
+    int b=30;
+	
+	int c=3;
+	
+	System.out.println(a+b-c);
+	
+  }
+  
+ }
+ 
+ 
