9eee37722079b1afe59bf692622709f9493407d3
souravpuniyani <souravpuniyani22@gmail.com>
2024-03-27 20:04:46 +0530

    java introduction

---
+++ SI.java
@@ -0,0 +1,25 @@
+package introduction;
+
+class SI
+{
+
+  public static void main(String args[])
+  { 
+    float P=2000.2f;
+	int r=10;
+	int t=2;
+	
+	int radius=2;
+	
+	int S= (int)( P*r*t/100);
+	System.out.println(S);
+	
+	float area= (3.14f*radius*radius);
+	System.out.println(area);
+	
+   }
+
+
+
+
+}
---
+++ package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author sourav
+ *
+ */
+package introduction;
